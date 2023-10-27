83e272b4e774cd48fe81f9f5f5a9fc2456019588
Matt <mattyh357@gmail.com>
2023-10-27 20:51:18 +0100

    Create Bluetooth.h
    Basic skeleton for BT class

---
+++ HudUnit/src/Bluetooth.h
@@ -0,0 +1,67 @@
+//
+// Created by matt on 26/10/23.
+//
+
+
+//https://github.com/nkolban/esp32-snippets/tree/master
+
+#pragma once
+
+#include <BLEServer.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+//#include "/home/matt/.arduino15/packages/esp32/hardware/esp32/2.0.11/libraries/BLE/src/BLEDevice.h"
+
+class Bluetooth {
+public:
+    Bluetooth() {};
+    Bluetooth(const char* deviceName, const char* serviceID, const char* charID);
+
+    void begin();
+    bool isConnected();
+    void setAdvertising(bool status);
+
+    void sendData(const std::string& data);
+    bool isThereNewData();
+    std::string getData();
+
+//    void getOOB();
+//    void clearLTK()
+
+private:
+    //TODO comments
+    void wipeData();
+
+
+    /** @brief The device name that is displayed during discovery. */
+    const char* _deviceName;
+
+    /** @brief The BLE server instance for managing BLE connections and services. */
+    BLEServer* _pServer;
+
+    /** @brief Represents a BLE characteristic which is a data container on the BLE server. */
+    BLECharacteristic* _pCharacteristic;
+
+    /** @brief Universally Unique Identifier for the BLE service. */
+    const char* _serviceUUID;
+
+    /** @brief Universally Unique Identifier for the BLE characteristic. */
+    const char* _charUUID;
+
+    /** @brief TODO Comment :) */
+    bool _newDataAvailable;
+
+
+
+
+
+};
+
+
+
+
+// TODO power management? Put into low power maybe?
+// TODO when receive data, check acknowledgement is required and if so, send it
+// TODO over-the-air updates?
