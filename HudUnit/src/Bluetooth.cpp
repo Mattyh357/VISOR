591e09413d3686f5c6737fc89c247c85ab21df6a
Matt <mattyh357@gmail.com>
2023-11-17 13:34:09 +0000

    Update Bluetooth.cpp
    Removing previous implementation for isThereNewData and raplacing with proper callback.
    
    additionally, adding bunch of code that I'm testing atm :)

--- HudUnit/src/Bluetooth.cpp
+++ HudUnit/src/Bluetooth.cpp
@@ -97,80 +97,128 @@ void Bluetooth::setAdvertising(bool status) {
 
 void Bluetooth::sendData(uint8_t data) {
     if(isConnected()) {
+        Serial.printf("Sending %02X ", data);
+        Serial.println();
         _pCharacteristic->setValue(&data, sizeof(data));
         _pCharacteristic->notify();
     }
 }
 
 bool Bluetooth::isThereNewData() {
-    _newDataAvailable = !(_pCharacteristic->getValue().empty());
     return _newDataAvailable;
 }
 
-std::string Bluetooth::getData() {
-    std::string newData = _pCharacteristic->getValue();
-    wipeData();
-    _newDataAvailable = false;  // Reset flag after reading.
 
-    return newData;
-}
 
-void Bluetooth::wipeData() {
-    _pCharacteristic->setValue("");
-}
 
 
 
 
 // TESTING
 
-
 void Bluetooth::onReceivedData(const uint8_t* data, size_t length) {
-    if (length < 3) return; // Ensure there's enough data for index and checksum_rec
 
+    if (length < 3) {
+        Serial.println("PROBLEM");
+        return; // TODO problem
+    }
+
+
+    //debug print all
+    Serial.print("RECEIVED bytes: ");
+    for (size_t i = 0; i < length - CHECKSUM_SIZE; ++i) {
+        Serial.printf("%02X ", data[i]); // Print each byte in hex format
+    }
+    Serial.println(); // New line after printing all bytes
+
+
+    //check checksum
+    // TODO do the do do
+    uint8_t checksum = data[length - CHECKSUM_SIZE];
+
+    if (checksum != calculateChecksum(data, length)) {
+        // Handle error
+        Serial.println("data error");
+        sendAcknowledgment(false);
+        return;
+    }
+
+
+    // If we're currently receiving a multi-chunk file
+    if (_remainingChunks > 0) {
+        handleFileChunk(data, length);
+        _remainingChunks--;
+
+        Serial.print("Remaining chunks: ");
+        Serial.println(_remainingChunks);
+        if (_remainingChunks == 0) {
+            Serial.println("CHUNKS ZERO");
+            onCompleteReceivedData(IMG_FILE_INSTRUCTION, 1);  // Call when all chunks are received
+        }
+    }
+    else {
+        // First byte as instruction
+        uint8_t instruction = data[0];
+        uint16_t payload = static_cast<uint16_t>(data[1]) << 8 | static_cast<uint16_t>(data[2]);
+
+
+        // TODO magic bytes
+        switch (instruction) {
+            case SPEED_INSTRUCTION:
+                // processNumber(data + 1, length - 1);
+                onCompleteReceivedData(SPEED_INSTRUCTION, payload);
+                break;
+            case NAVIGATION_IMG_INSTRUCTION:
+                onCompleteReceivedData(NAVIGATION_IMG_INSTRUCTION, payload);
+                break;
+            case IMG_FILE_INSTRUCTION: // File Transfer
+                _remainingChunks = payload;
+                _fileStorage.clear(); // Clear any existing data
+                _fileStorage.reserve(16000); // Reserve space for the file
+
+                Serial.println(("Starting file transfer" + std::to_string(_remainingChunks)).c_str());
+                break;
+
+            case ALL_BOOT_DATA_SENT:
+                onCompleteReceivedData(ALL_BOOT_DATA_SENT, payload);
+                break;
+
+            default:
+                Serial.println("UNKNOWN INSTRUCTION!!!!");
+                // Handle unknown instruction
+                // TODO not sure tbh
+                return;
+                break;
+        }
+    }
+
+    // I got here ... that means where ever was happening was supposed to
+
+    sendAcknowledgment(true);
+
+}
+
+void Bluetooth::handleFileChunk(const uint8_t* data, size_t length) {
     // Extract index (first 2 bytes)
-    uint16_t index = data[0] | (data[1] << 8);
 
     // Extract actual data (between index and checksum_rec)
     std::vector<uint8_t> actualData(data + 2, data + length - 1);
 
-    Serial.print("Data bytes: ");
-    for (auto byte : actualData) {
-        Serial.printf("%02X ", byte);
-    }
-    Serial.println(); // Move to a new line after printing
+//    Serial.print("processing bytes: ");
+//    for (auto byte : actualData) {
+//        Serial.printf("%02X ", byte);
+//    }
+//    Serial.println();
 
-
-    // checksum_rec (last byte)
-    uint8_t checksum_rec = data[length - 1];
-    uint8_t checksum_cal = calculateChecksum(actualData.data(), actualData.size());
-
-
-    std::string msg = "index: " + std::to_string(index) + "- " +std::to_string(checksum_rec) + "==" + std::to_string(checksum_cal);
-    Serial.println(msg.c_str());
-
-    if (checksum_rec == checksum_cal) {
-//        storeData(index, actualData);
-        Serial.println("data ok");
-        sendAcknowledgment(true);
-    } else {
-        // Handle error
-        Serial.println("data error");
-        sendAcknowledgment(false);
-    }
+    _fileStorage.insert(_fileStorage.end(), actualData.begin(), actualData.end());
+//    _fileStorage.insert(_fileStorage.end(), chunkData + 2, chunkData + length - 1);
 }
-
-
-
 
 uint8_t Bluetooth::calculateChecksum(const uint8_t* data, size_t length) {
     uint8_t calculatedChecksum = 0;
-    Serial.print("Data bytes: ");
-    for (size_t i = 0; i < length; ++i) {
+    for (size_t i = 0; i < length - CHECKSUM_SIZE; ++i) {
         calculatedChecksum += data[i];
-        Serial.printf("%02X ", data[i]); // Print each byte in hex format
     }
-    Serial.println(); // New line after printing all bytes
     return calculatedChecksum;
 }
 
@@ -178,4 +226,29 @@ uint8_t Bluetooth::calculateChecksum(const uint8_t* data, size_t length) {
 void Bluetooth::sendAcknowledgment(bool isSuccessful) {
     delay(50); //just to be sure
     sendData(isSuccessful ? 0x01 : 0x00);
+}
+
+
+
+void Bluetooth::onCompleteReceivedData(uint8_t instruction, uint16_t data) {
+    _newDataAvailable = true;
+    last_instruction = instruction;
+
+    std::string msg = "COMPLETE MESSAGE: " + std::to_string(instruction) + " - " + std::to_string(data);
+    Serial.println(msg.c_str());
+
+    last_data = data;
+
+    if(instruction == IMG_FILE_INSTRUCTION) {
+        Serial.print("Complete Data bytes: ");
+        for (auto byte : _fileStorage) {
+            Serial.printf("%02X ", byte);
+        }
+        Serial.println(); // Move to a new line after printing
+    }
+}
+
+uint16_t Bluetooth::getData() {
+    _newDataAvailable = false;  // Reset flag after reading.
+    return last_data;
 }
