d5c250729891f68b1e2f7d9d682e1134b46bdf36
Matt <mattyh357@gmail.com>
2024-02-02 11:33:03 +0000

    Implementing permission checker

---
+++ AndroidApp/visor/app/src/main/java/com/matt/visor/PermissionHelper.java
@@ -0,0 +1,153 @@
+package com.matt.visor;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class PermissionHelper {
+
+    private static final int PERMISSION_ALL = 1;
+
+    private Context _context;
+    private List<String> _list;
+    private Callback _callback;
+
+    private boolean _showInfoDialog = true;
+
+
+    /**
+     * Permission helper
+     *
+     * @param context   The context of the application.
+     * @param list      List of permissions to be checked
+     * @param callback  Callback
+     */
+    public PermissionHelper(Context context, List<String> list, Callback callback) {
+        _context = context;
+        _list = list;
+        _callback = callback;
+    }
+
+
+    /**
+     * Iterates over the list of permissions, checks for each and if permission is NOT granted, call for the request.
+     * If all permissions are granted, onAllPermissionsGranted callback is invoked.
+     */
+    public void start() {
+        Iterator<String> iterator = _list.iterator();
+        while(iterator.hasNext()) {
+            String permission = iterator.next();
+
+            System.out.println("Checking: " + permission);
+
+            if(!hasPermissions(permission)) {
+                System.out.println("Requesting: " + permission);
+                requestNewPermission(permission);
+                return;
+            }
+            else {
+                iterator.remove();
+                System.out.println("Removing: " + permission);
+            }
+        }
+
+        System.out.println("All permissions are ok");
+        _callback.onAllPermissionsGranted();
+    }
+
+
+    /**
+     * Callback for the result from requesting permissions
+     *
+     * @param requestCode The request code passed in requestPermissions
+     * @param permissions The requested permissions. Never null.
+     * @param grantResults  The grant results for the corresponding permissions which is either PERMISSION_GRANTED or PERMISSION_DENIED. Never null.
+     */
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        System.out.println("onRequestPermissionsResult!");
+
+        if(requestCode == PERMISSION_ALL && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            System.out.println("Permission granted: " + permissions[0]);
+            start();
+            return;
+        }
+
+        System.out.println("Permission unknown or denied");
+        _callback.onPermissionDenied(permissions[0]);
+    }
+
+
+    /**
+     * Checks if specified permission is granted.
+     *
+     * @param permissions The permission to check.
+     * @return True if the permission is granted, false otherwise.
+     */
+    private boolean hasPermissions(String permissions) {
+        if (_context != null && permissions != null) {
+            return ActivityCompat.checkSelfPermission(_context, permissions) == PackageManager.PERMISSION_GRANTED;
+        }
+        return true;
+    }
+
+
+    /**
+     * Requests a permission, if this is called for the first time, also displays infoDialog
+     *
+     * @param permission name of the permission
+     */
+    private void requestNewPermission(String permission) {
+        if(_showInfoDialog) {
+            _showInfoDialog = false;
+            showInfoDialog(permission);
+        }
+        else {
+            ActivityCompat.requestPermissions((Activity)_context, new String[]{permission}, PERMISSION_ALL);
+        }
+    }
+
+
+    /**
+     * Displays info dialog, informing user that the app will require some permissions
+     *
+     * @param permission name of the permission
+     */
+    private void showInfoDialog(String permission) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(_context, R.style.MyDialogTheme);
+
+        builder.setTitle(R.string.dialog_confirm);
+        builder.setMessage(R.string.permission_request);
+        builder.setCancelable(false);
+
+        builder.setPositiveButton(R.string.yes, (dialog, which) -> {
+            dialog.dismiss();
+            ActivityCompat.requestPermissions((Activity)_context, new String[]{permission}, PERMISSION_ALL);
+        });
+
+        builder.setNegativeButton(R.string.no, (dialog, which) -> {
+            dialog.dismiss();
+            _callback.onPermissionDenied(permission);
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+
+    /**
+     * Callback interface
+     */
+    public interface Callback {
+        void onAllPermissionsGranted();
+        void onPermissionDenied(String permission);
+
+    }
+
+}
--- AndroidApp/visor/app/src/main/java/com/matt/visor/SplashScreen.java
+++ AndroidApp/visor/app/src/main/java/com/matt/visor/SplashScreen.java
@@ -1,12 +1,27 @@
+/**
+ * This class is part of the V.I.S.O.R app.
+ * Splash screen - Responsible for loading saved configuration and devices
+ *
+ * TODO AI image!
+ *
+ * @version 1.0
+ * @since 0/01/2024
+ */
+
 package com.matt.visor;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ProgressBar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.matt.visor.app.VisorApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -15,6 +30,8 @@ public class SplashScreen extends AppCompatActivity {
     @SuppressWarnings("FieldCanBeLocal")
     private final int WAIT_TIME = 2100;
 
+    private PermissionHelper _permissionHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,16 +39,45 @@ public class SplashScreen extends AppCompatActivity {
 
         _progressBar = findViewById(R.id.splash_progressBar);
 
-        System.out.println("Splash onCreate");
 
+        // Permissions
+        List<String> listOfPermissions = new ArrayList<>();
+        // Map
+        listOfPermissions.add(android.Manifest.permission.ACCESS_COARSE_LOCATION);
+        listOfPermissions.add(android.Manifest.permission.ACCESS_FINE_LOCATION);
+        listOfPermissions.add(android.Manifest.permission.INTERNET);
 
+        // Bluetooth
+        listOfPermissions.add(android.Manifest.permission.BLUETOOTH);
+        listOfPermissions.add(android.Manifest.permission.BLUETOOTH_ADMIN);
+        listOfPermissions.add(android.Manifest.permission.BLUETOOTH_CONNECT);
+        listOfPermissions.add(Manifest.permission.BLUETOOTH_SCAN);
+
+        //Permission helper
+        _permissionHelper = new PermissionHelper(this, listOfPermissions, new PermissionHelper.Callback() {
+            @Override
+            public void onAllPermissionsGranted() {
+                System.out.println("all ok :) please continue");
+                startLoading();
+            }
+            public void onPermissionDenied(String permission) {
+                showDeniedDialog(permission);
+            }
+        });
+
+        _permissionHelper.start();
+    }
+
+    /**
+     *  Instantiates app loading process on a separate thread.
+     */
+    private void startLoading() {
         new Thread(() -> {
             loadStuff();
             waitForNoReason();
             startApp();
             finish();
         }).start();
-
     }
 
 
@@ -42,6 +88,35 @@ public class SplashScreen extends AppCompatActivity {
         VisorApplication app = (VisorApplication) getApplication();
         app.loadDevices(this);
 //        app.loadListOfJourneys(this);
+    }
+
+
+    /**
+     * Callback for the result from requesting permissions
+     *
+     * @param requestCode The request code passed in requestPermissions
+     * @param permissions The requested permissions. Never null.
+     * @param grantResults  The grant results for the corresponding permissions which is either PERMISSION_GRANTED or PERMISSION_DENIED. Never null.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        _permissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    /**
+     * Displays a dialog indicating a permission has been denied and that app will not work.
+     *
+     * @param permission The name of the permission that was denied.
+     */
+    private void showDeniedDialog(String permission) {
+        System.out.println("Permission DENIED: " + permission);
+        androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(this);
+        builder.setMessage(R.string.permission_denied)
+                .setCancelable(false)
+                .setPositiveButton("OK", (dialog, id) -> {});
+        androidx.appcompat.app.AlertDialog alert = builder.create();
+        alert.show();
     }
 
 
