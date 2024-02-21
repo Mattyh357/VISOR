5f96d73d552c076da48c36259b8d5526df731625
Matt <mattyh357@gmail.com>
2024-02-21 20:21:04 +0000

    Create Step.java

---
+++ AndroidApp/visor/app/src/main/java/com/matt/visor/GoogleMap/Step.java
@@ -0,0 +1,17 @@
+package com.matt.visor.GoogleMap;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class Step {
+    public String instruction;
+    public String distanceText;
+    public int distanceValue;
+    public String durationText;
+    public int durationValue;
+    public LatLng startLocation;
+    public LatLng endLocation;
+    public String maneuver;
+
+
+}
+
