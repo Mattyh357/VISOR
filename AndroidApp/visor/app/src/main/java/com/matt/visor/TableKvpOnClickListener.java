c11c803b7ee86bc3e6059b1df0f2791dd0713617
Matt <mattyh357@gmail.com>
2024-02-22 12:00:40 +0000

    Create TableKvpOnClickListener.java

---
+++ AndroidApp/visor/app/src/main/java/com/matt/visor/TableKvpOnClickListener.java
@@ -0,0 +1,13 @@
+package com.matt.visor;
+
+public interface TableKvpOnClickListener {
+
+    /**
+     * Interface for handling item click events in a table key-value pair list.
+     *
+     * @param item The item that was clicked.
+     */
+    void onItemClick(TableKvpItem item);
+
+
+}
