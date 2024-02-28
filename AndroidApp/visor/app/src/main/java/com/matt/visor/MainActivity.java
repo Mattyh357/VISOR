9e4a040beae8c078c0eccf2e6ef506085a91b6eb
Scotchi <mattyh357@gmail.com>
2024-02-28 10:04:57 +0000

    Update MainActivity.java
    Binding now local variable

--- AndroidApp/visor/app/src/main/java/com/matt/visor/MainActivity.java
+++ AndroidApp/visor/app/src/main/java/com/matt/visor/MainActivity.java
@@ -24,13 +24,11 @@ import com.matt.visor.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        com.matt.visor.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         // AppBar
