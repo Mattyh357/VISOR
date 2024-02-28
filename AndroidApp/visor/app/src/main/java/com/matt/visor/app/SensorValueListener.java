31489042546c74bbc2b2d26d6823e64a1dd5abfc
Scotchi <mattyh357@gmail.com>
2024-02-28 10:20:15 +0000

    Update SensorValueListener.java
    Adding comments

--- AndroidApp/visor/app/src/main/java/com/matt/visor/app/SensorValueListener.java
+++ AndroidApp/visor/app/src/main/java/com/matt/visor/app/SensorValueListener.java
@@ -1,6 +1,18 @@
+/**
+ * This class is part of the V.I.S.O.R app.
+ * The SensorValueListener contains methods which are called when sensor values changes.
+ *
+ * @version 1.0
+ * @since 21/02/2024
+ */
+
 package com.matt.visor.app;
 
 public interface SensorValueListener {
+
+    /**
+     * Called when sensor values changes
+     */
     void onSensorValueChange();
 
 }
