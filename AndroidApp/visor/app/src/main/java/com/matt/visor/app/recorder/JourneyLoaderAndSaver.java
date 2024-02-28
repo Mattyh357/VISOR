9eae813d930f546e88e5151c4ac97240b3b78382
Scotchi <mattyh357@gmail.com>
2024-02-28 09:58:22 +0000

    Update JourneyLoaderAndSaver.java
    History now in correct order

--- AndroidApp/visor/app/src/main/java/com/matt/visor/app/recorder/JourneyLoaderAndSaver.java
+++ AndroidApp/visor/app/src/main/java/com/matt/visor/app/recorder/JourneyLoaderAndSaver.java
@@ -23,6 +23,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -42,6 +43,8 @@ public class JourneyLoaderAndSaver {
         for(String journeyID : getAllSubfolder(JOURNEY_FOLDER, context)) {
             list.add(getJourneyById(journeyID, context));
         }
+
+        Collections.reverse(list);
 
         return list;
     }
