bc0fa931653df69d5ceb97093901ac6d66ad1173
Scotchi <mattyh357@gmail.com>
2024-02-28 10:20:12 +0000

    Update SensorStatusListener.java
    Adding comments

--- AndroidApp/visor/app/src/main/java/com/matt/visor/app/SensorStatusListener.java
+++ AndroidApp/visor/app/src/main/java/com/matt/visor/app/SensorStatusListener.java
@@ -1,9 +1,19 @@
+/**
+ * This class is part of the V.I.S.O.R app.
+ * The SensorStatusListener contains methods which are called when sensor status changes.
+ *
+ * @version 1.0
+ * @since 21/02/2024
+ */
+
 package com.matt.visor.app;
 
 public interface SensorStatusListener {
 
 
-    // TODO what the status actually is? :D cos this is a bit stupid tbh :D
+    /**
+     * Called when sensor status changes
+     */
     void onChange();
 
 }
