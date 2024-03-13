6e03c1305d93747396793f44f9ee70babff447ed
Scotchi <mattyh357@gmail.com>
2024-03-13 16:07:50 +0000

    Update Visor.cpp

--- HudUnit/src/Visor.cpp
+++ HudUnit/src/Visor.cpp
@@ -1,21 +1,12 @@
-//
-// Created by matt on 27/10/23.
-//
+/**
+ *  @file Visor.cpp
+ *  @brief Implementation file for class: Visor
+ *
+ *  @date 27/10/2023
+ *  @bug No known bugs.
+ */
 
 #include "Visor.h"
-
-// TODO NOTE this whole file is a work in progress! or rather, it's currently ONLY USED FOR TESTING!!!!
-
-
-enum operation_mode {
-    disconnected,
-    booting,
-    working,
-    lostSignal,
-};
-
-
-operation_mode myMode;
 
 void Visor::setup() {
     Serial.begin(SERIAL_BOUD);
@@ -27,149 +18,85 @@ void Visor::setup() {
 
     // Display
     _display = Display(DISPLAY_WIDTH, DISPLAY_HEIGHT, PIN_CS, PIN_DC, PIN_MOSI, PIN_SCK, PIN_RST);
+    _display.setTextBackgroundColor(BLACK);
+    _display.setTextColor(WHITE);
+    _display.setTextFont(&Font20);
+
     _display.begin();
     _display.fillScreen(BLACK);
 
-    // Start with disconnected image
-    _display.drawImageTest(MyImage(ImgDisconnected.Width, ImgDisconnected.Height, ImgDisconnected.toVector()));
-
-    myMode = disconnected;
-
-
-
-    // TEST CODE
-
+    // Start as disconnected
+    handle_disconnect();
 }
 
 
-bool _bootingCompleted = false;
-
-// modes: disconnected -> loading imaged -> loading_config-> working/lost_connection
 
 void Visor::update() {
     if(_updateTimer > millis())
         return;
-
     _updateTimer = millis() + UPDATE_INTERVAL;
 
-//    return; // TODO remove :)
-
-    // lost connection handling
-    if(myMode != disconnected && myMode != lostSignal && _bt.isConnected() == false) {
-        // TODO should not print every loop.. just once
-        // when I lose connection
-        _display.fillScreen(BLACK);
-        _display.drawImageTest(MyImage(ImgDisconnected.Width, ImgDisconnected.Height, ImgDisconnected.toVector()));
-        myMode = lostSignal;
-        _updateTimer = 0;
+    // Disconnected
+    if(_bt.isConnected() == false) {
+        handle_disconnect();
         return;
     }
 
-    // first thing...just wait for it to connect. and send request for boot data
-    if(myMode == disconnected){
-        if(_bt.isConnected() == true){
-            // TODO keep trying to request it
-            Serial.println("Connected - sending REQUEST_BOOT_DATA");
-            delay(1000);
-            myMode = booting;
-            _display.fillScreen(BLACK);
-            _display.drawImageTest(MyImage(ImgDisconnected.Width, ImgDisconnected.Height, ImgDisconnected.toVector()));
-            _bt.sendData(REQUEST_BOOT_DATA);
-        }
-        return;
-    }
-    else if (myMode == booting) {
-        // Print % of booting
-        _display.print(10, 5, "BOOT", &Font24, BLACK, WHITE);
-
-        std::string status = std::to_string((_bt._totalChunks - _bt._remainingChunks)) + "/" + std::to_string(_bt._totalChunks);
-        _display.print(10, 30, status.c_str(), &Font24, BLACK, WHITE);
-
-        // Booting completed
-        if(_bt.last_instruction == ALL_BOOT_DATA_SENT) {
-            Serial.println("PROCESSING BOOT DATA");
-            _imgProcessor = ImageProcessor(_bt._fileStorage);
-
-            delay(200);
-            // TODO clear _fileStorage
-            Serial.println("FINISHED processing");
-
-            _bootingCompleted = true;
-            myMode = working;
-            _display.fillScreen(BLACK);
-            _display.drawImageTest(MyImage(ImgStandBy.Width, ImgStandBy.Height, ImgStandBy.toVector()));
-            _bt.sendData(BOOT_DATA_PROCESSED);
-        }
-
-        return;
-    }
-    else if (myMode == lostSignal) {
-        if(_bt.isConnected() == true) {
-            if(_bootingCompleted) {
-                myMode = working;
-                _display.fillScreen(BLACK);
-                _display.drawImageTest(MyImage(ImgStandBy.Width, ImgStandBy.Height, ImgStandBy.toVector()));
-            }
-            else{
-                myMode = booting;
-                _display.fillScreen(BLACK);
-                _display.print(10, 10, "b1", &Font24, BLACK, WHITE); // TODO replace with img
-                _bt.sendData(REQUEST_BOOT_DATA);
-            }
-
-            _updateTimer = 0;
-            return;
-        }
-    }
-
-
-    //if I'm here, that meaning I'm in working state
-
+    // Connected and there might be something to display
     if(_bt.isThereNewData()) {
+        handle_workMode(_bt.getData());
+    }
+}
 
-        Serial.print("New BT data received: ");
 
-        if(_bt.last_instruction == SPEED_INSTRUCTION) {
-            std::string number = std::to_string(_bt.getData());
-            Serial.println(number.c_str());
+void Visor::handle_disconnect() {
+    clearOnDifferentInstruction(-1);
 
-            if(_prevPrintType != _bt.last_instruction){
-                _display.fillScreen(BLACK);
-                _prevPrintType = _bt.last_instruction;
-            }
+    _display.drawImage(&ImgDisconnected);
+    _display.print(-1, 20, "Disconnected");
+}
 
-            _display.print(30, 15, number.c_str(), &Font24, BLACK, WHITE);
+void Visor::handle_workMode(std::vector<std::string> data) {
+
+    int instruction = std::stoi(data[0]);
+
+    // Clear screen if needed
+    clearOnDifferentInstruction(instruction);
+
+    switch (instruction) {
+        case INSTRUCTION_SPEED_TEST: {
+            Serial.println("SPEED");
+            std::string speed = data[1];
+            std::string distance = data[2];
+
+            _display.print(0, 15, "(s)", &Font8, BLACK, WHITE);
+            _display.print(-1, 20, speed.c_str());
+
+            _display.print(0, 45, "(d)", &Font8, BLACK, WHITE);
+            _display.print(-1, 40, distance.c_str());
+
+            break;
+        }
+        case INSTRUCTION_NAVIG_TEST: {
+            uint8_t iconNumber = std::stoi(data[1]);
+            std::string distance = data[2];
+
+            _display.drawImage(NavIcon::getImage(iconNumber));
+
+            _display.fillRect(30, 40, 70, 55, BLACK);
+            _display.print(-1, 40, distance.c_str());
+        }
+            break;
+        default:
+            Serial.println("Unknown instruction");
             return;
-        }
-        else if (_bt.last_instruction == NAVIGATION_IMG_INSTRUCTION) {
-            uint16_t number = _bt.getData();
+    }
+}
 
-            Serial.print("nav test image: ");
-            for (auto byte : _imgProcessor.getImage(number).getData()) {
-                Serial.printf("%02X ", byte);
-            }
-            Serial.println(); // Move to a new line after printing
-
-            std::string text = "I:" + std::to_string(number);
-            Serial.println(text.c_str());
-
-            if(_prevPrintType != _bt.last_instruction){
-                _display.fillScreen(BLACK);
-                _prevPrintType = _bt.last_instruction;
-            }
-
-            _display.drawImageTest(_imgProcessor.getImage(number));
-
-
-            return;
-        }
-        else {
-            std::string number = std::to_string(_bt.getData());
-            std::string text = "unknown ins:" + std::to_string(_bt.last_instruction) + "data: " + number;
-            Serial.println(text.c_str());
-
-        }
-
+void Visor::clearOnDifferentInstruction(int instruction) {
+    if(_lastInstruction != instruction){
+        _display.fillScreen(BLACK);
+        _lastInstruction = instruction;
     }
 }
 
