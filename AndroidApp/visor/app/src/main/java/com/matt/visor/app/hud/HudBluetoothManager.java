a1288c29a5f6cf024d1aab82fe38b4cec6aa5511
Scotchi <mattyh357@gmail.com>
2024-03-19 10:56:25 +0000

    Update HudBluetoothManager.java

--- AndroidApp/visor/app/src/main/java/com/matt/visor/app/hud/HudBluetoothManager.java
+++ AndroidApp/visor/app/src/main/java/com/matt/visor/app/hud/HudBluetoothManager.java
@@ -24,18 +24,18 @@ import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothProfile;
 import android.content.Context;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.LinkedList;
 import java.util.Optional;
-import java.util.Queue;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.UUID;
 
+//TODO resend on invalid ack
+//TODO resend on invalid ack
+//TODO resend on invalid ack
+//TODO resend on invalid ack
+
+
 public class HudBluetoothManager {
-    private final Activity _activity;
 
     // Bluetooth
     private static String _macAddress;
@@ -47,26 +47,20 @@ public class HudBluetoothManager {
 
     // Ready
     private boolean _connected = false;
-    private boolean _booted = false;
+
 
 
     // Chunks
     private final int DEFAULT_CHUNK_SIZE = 20;
-    private final int OPTIMAL_CHUNK_SIZE = 100; // TODO better number
+    private final int OPTIMAL_CHUNK_SIZE = 100; // Should be more than enough
     private int _chunkSize = DEFAULT_CHUNK_SIZE;
-    private Queue<byte[]> _remainingChunks = new LinkedList<>();
 
     // Listener
     private final HudBluetoothListener _listener;
 
-
     // Timers
     private Timer ackTimeoutTimer;
     private final long ACK_TIMEOUT = 1000;
-
-
-
-
 
     /**
      * Initializes Bluetooth connection parameters.
@@ -81,7 +75,6 @@ public class HudBluetoothManager {
         _macAddress = macAddress;
         _serviceUUID = UUID.fromString(sUUID);
         _characteristicUUID = UUID.fromString(cUUID);
-        _activity = activity;
         _listener = listener;
 
         // Init bluetooth
@@ -100,9 +93,7 @@ public class HudBluetoothManager {
         System.out.println("Trying to connect");
         BluetoothDevice device = _bluetoothAdapter.getRemoteDevice(_macAddress);
         if (device != null) {
-            System.out.println("here");
             _bluetoothGatt = device.connectGatt(activity, true, gattCallback);
-
             Optional.ofNullable(_listener).ifPresent(HudBluetoothListener::onConnecting);
         }
     }
@@ -122,91 +113,61 @@ public class HudBluetoothManager {
         _connected = false;
     }
 
-    /**
-     * Sends data to the connected Bluetooth device.
-     *
-     * @param instruction The instruction byte to send.
-     * @param number The integer data to send, split into bytes.
-     */
-    public void sendData(byte instruction, int number) {
-        if(!_connected || !_booted)
-            return;
 
-        _remainingChunks.clear();
-
-        byte[] data = new byte[3];
-        data[0] = instruction;
-        data[1] = (byte) ((number >> 8) & 0xff);
-        data[2] = (byte) (number & 0xff);
-
-        _remainingChunks.add(addChecksum(data));
-        sendNextChunk();
-    }
-
-
-
-    // TODO fix this whole thing....
-    public void sendBootData() {
-
+    public void sendDataTest(int instruction, String payload) {
         if(!_connected)
             return;
 
-        //TODO CRAP here
+        String dataToSend = instruction + "," + payload;
+        byte[] data = dataToSend.getBytes();
 
-        File file = new File(_activity.getFilesDir(), "images.cbi");
+        byte[] withChecksum = addChecksum(data);
 
-        byte[] data1 = new byte[(int) file.length()];
-        FileInputStream fis = null;
-        try {
-            fis = new FileInputStream(file);
-            fis.read(data1);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (fis != null) {
-                try {
-                    fis.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        writeDataTest(withChecksum);
+        // TODO save somewhere and remove on ok ack , resend on false ack
+    }
+
+
+    /**
+     * Writes data to the Bluetooth device if connected and service is available.
+     *
+     * @param data The byte array data to write to the device.
+     */
+    @SuppressLint("MissingPermission")
+    private void writeDataTest(byte[] data) {
+        System.out.println("wirting test Data");
+        // Stop if there is no gatt
+        if(_bluetoothGatt == null){
+            System.out.println("Write data - GATT is null");
+            return;
+        }
+
+        // Write
+        BluetoothGattService service = _bluetoothGatt.getService(_serviceUUID);
+        if (service != null) {
+            BluetoothGattCharacteristic characteristic = service.getCharacteristic(_characteristicUUID);
+            if (characteristic != null) {
+                characteristic.setValue(data);
+                _bluetoothGatt.writeCharacteristic(characteristic);
             }
         }
-        //TODO CRAP here
-
-        byte[] fileData = data1;
-
-        System.out.println("sending big data :)");
-
-        // TODO magic
-        int headerSize = 3; // 2 bytes for index and 1 byte for checksum
-        int dataSize = _chunkSize - headerSize;
-        int totalChunks = (int) Math.ceil((double) fileData.length / dataSize);
-
-        // TODO shouldn't clear here -
-        _remainingChunks.clear();
-
-        // Instruction chunk
-        byte[] data = new byte[3];
-        data[0] = InstructionsByte.IMG_FILE_INSTRUCTION;
-        data[1] = (byte) ((totalChunks >> 8) & 0xff);
-        data[2] = (byte) (totalChunks & 0xff);
-
-        _remainingChunks.add(addChecksum(data));
-
-        // add FILE chunks
-        addFileChunks(fileData, totalChunks);
-
-        //add config files
-
-        // Add last ALL_BOOT_DATA_SENT instruction
-        data[0] = InstructionsByte.ALL_BOOT_DATA_SENT;
-        data[1] = (byte) (0);
-        data[2] = (byte) (0);
-        _remainingChunks.add(addChecksum(data));
-
-        // All good ;)
-        sendNextChunk();
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     /**
      * Starts a timer to handle acknowledgment timeout for chunk resending.
@@ -268,84 +229,7 @@ public class HudBluetoothManager {
         return dataWithChecksum;
     }
 
-    /**
-     * Splits data into chunks, adds indices, checksums, and queues them for sending.
-     *
-     * @param data The data to be split into chunks.
-     * @param totalChunks The total number of chunks to create.
-     */
-    private void addFileChunks(byte[] data, int totalChunks) {
-        int headerSize = 3; // 2 bytes for index and 1 byte for checksum
-        int dataSize = _chunkSize - headerSize;
 
-        for (int i = 0; i < totalChunks; i++) {
-            int start = i * dataSize;
-
-            if (start >= data.length) {
-                break; // Stop if 'start' is out of the data array bounds
-            }
-
-            int end = Math.min(start + dataSize, data.length);
-
-            // Create chunk data including the index
-            byte[] chunkData = new byte[end - start + 2]; // +2 for the index
-            chunkData[0] = (byte) (i & 0xff);          // Lower byte of the index
-            chunkData[1] = (byte) ((i >> 8) & 0xff);   // Upper byte of the index
-
-            // Copy actual data into chunkData after index
-            System.arraycopy(data, start, chunkData, 2, end - start);
-
-            byte[] withChecksum = addChecksum(chunkData);
-
-            // Add checksum and add the chunk to the queue
-            _remainingChunks.add(withChecksum);
-        }
-    }
-
-    /**
-     * Writes data to the Bluetooth device if connected and service is available.
-     *
-     * @param data The byte array data to write to the device.
-     */
-    @SuppressLint("MissingPermission")
-    private void writeData(byte[] data) {
-        // Stop if there is no gatt
-        if(_bluetoothGatt == null){
-            System.out.println("Write data - GATT is null");
-            return;
-        }
-
-        // Stop if not connected
-        if (!_connected) {
-            System.out.println("Not connected");
-            return;
-        }
-
-        // Write
-        BluetoothGattService service = _bluetoothGatt.getService(_serviceUUID);
-        if (service != null) {
-            BluetoothGattCharacteristic characteristic = service.getCharacteristic(_characteristicUUID);
-            if (characteristic != null) {
-                characteristic.setValue(data);
-                _bluetoothGatt.writeCharacteristic(characteristic);
-            }
-        }
-    }
-
-    /**
-     * Sends the next chunk from the queue and starts the acknowledgment timeout timer.
-     */
-    private void sendNextChunk() {
-        // Remove last one
-        byte[] data = _remainingChunks.peek();
-
-        if(data == null)
-            return;
-
-        //send data and start timer
-        writeData(data);
-        startAckTimeoutTimer();
-    }
 
 
 
@@ -365,37 +249,37 @@ public class HudBluetoothManager {
 
         // Can received: ACK_OK, ACK_ERROR. REQUEST_BOOT_DATA, RESTART_BOOTING, BOOT_DATA_PROCESSED
 
-        if(value == InstructionsByte.ACK_OK) {
-            System.out.println("ACK OK! removing first chunk and moving on");
-            _remainingChunks.poll();
-
-            if(_remainingChunks.isEmpty()) {
-                // TODO DO SOMETHING :)
-                System.out.println("HAVE NO MORE DATA TO SEND");
-                cancelAckTimeoutTimer();
-                Optional.ofNullable(_listener).ifPresent(HudBluetoothListener::onAllDataSent);
-            } else {
-                sendNextChunk();
-            }
-        }
-        else if(value == InstructionsByte.ACK_ERROR) {
-            System.out.println("DATA WAS NOT OK!!!! not removing first and sending it again");
-            sendNextChunk();
-        }
-        else if (value == InstructionsByte.REQUEST_BOOT_DATA){
-            System.out.println("START BLASTING BOOT DATA - TODO automate");
-            Optional.ofNullable(_listener).ifPresent(HudBluetoothListener::onRequestBootData);
-            sendBootData();
-            _booted = false;
-        }
-        else if (value == InstructionsByte.BOOT_DATA_PROCESSED){
-            System.out.println("BOOTING Complete");
-            Optional.ofNullable(_listener).ifPresent(HudBluetoothListener::onBootComplete);
-            _booted = true;
-        }
-        else{
-            System.out.println("receivedUnknownInstruction...");
-        }
+//        if(value == InstructionsByte.ACK_OK) {
+//            System.out.println("ACK OK! removing first chunk and moving on");
+//            _remainingChunks.poll();
+//
+//            if(_remainingChunks.isEmpty()) {
+//                // TODO DO SOMETHING :)
+//                System.out.println("HAVE NO MORE DATA TO SEND");
+//                cancelAckTimeoutTimer();
+//                Optional.ofNullable(_listener).ifPresent(HudBluetoothListener::onAllDataSent);
+//            } else {
+//                sendNextChunk();
+//            }
+//        }
+//        else if(value == InstructionsByte.ACK_ERROR) {
+//            System.out.println("DATA WAS NOT OK!!!! not removing first and sending it again");
+//            sendNextChunk();
+//        }
+//        else if (value == InstructionsByte.REQUEST_BOOT_DATA){
+//            System.out.println("START BLASTING BOOT DATA - TODO automate");
+//            Optional.ofNullable(_listener).ifPresent(HudBluetoothListener::onRequestBootData);
+//            sendBootData();
+//            _booted = false;
+//        }
+//        else if (value == InstructionsByte.BOOT_DATA_PROCESSED){
+//            System.out.println("BOOTING Complete");
+//            Optional.ofNullable(_listener).ifPresent(HudBluetoothListener::onBootComplete);
+//            _booted = true;
+//        }
+//        else{
+//            System.out.println("receivedUnknownInstruction...");
+//        }
     }
 
 
@@ -424,8 +308,10 @@ public class HudBluetoothManager {
             if (newState == BluetoothProfile.STATE_CONNECTED) {
                 _connected = true;
                 gatt.discoverServices();
+                Optional.ofNullable(_listener).ifPresent(HudBluetoothListener::onConnected);
             } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
                 _connected = false;
+                Optional.ofNullable(_listener).ifPresent(HudBluetoothListener::onDisconnected);
             }
         }
 
