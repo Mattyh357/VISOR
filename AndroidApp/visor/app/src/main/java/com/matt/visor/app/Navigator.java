9193b8d2fe6be0f5e02c1559f25df5d89fd61216
Scotchi <mattyh357@gmail.com>
2024-03-19 10:54:40 +0000

    Update Navigator.java
    Refactor

--- AndroidApp/visor/app/src/main/java/com/matt/visor/app/Navigator.java
+++ AndroidApp/visor/app/src/main/java/com/matt/visor/app/Navigator.java
@@ -1,4 +1,14 @@
-// https://github.com/google/material-design-icons/issues/341
+/**
+ * This class is part of the V.I.S.O.R app.
+ * Manages navigation for a given route (list of steps retrieved from Google Direction API).
+ * Updates the current step based on provided location, and return distance to the next step as well
+ * as the string predetermined ID of the maneuver.
+ * The list of directions for used in Google API maneuver taken from:
+ * LINK: https://github.com/google/material-design-icons/issues/341
+ *
+ * @version 1.0
+ * @since 21/02/2024
+ */
 
 package com.matt.visor.app;
 
@@ -11,10 +21,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class NavigatorThingy {
-
-
-
+public class Navigator {
 
     private static final Map<String, Integer> MANEUVER_ID = new HashMap<>();
     private static final String DESTINATION_STR = "destination";
@@ -45,33 +52,34 @@ public class NavigatorThingy {
 
 
     // TODO test what number is adequate
-    private static final double THRESHOLD = 50; // Threshold in meters
-    private List<Step> _steps;
+    private static final double THRESHOLD = 50; // Seems to work just find
+    private final List<Step> _steps;
     private int _currentStepIndex = 0;
-    private boolean _changed;
-
     private double _distanceToNext = 0;
 
-
-    public NavigatorThingy(List<Step> steps) {
-
+    /**
+     * Initializes navigation with a predefined set of steps.
+     *
+     * @param steps The list of navigation steps to follow.
+     */
+    public Navigator(List<Step> steps) {
         // TODO hardcoded step
-        System.out.println("");
-
         steps = getHardcodedSteps();
-
-        this._steps = getHardcodedSteps();
-
-        System.out.println("TEST: " + getManeuverString());
-
+        _steps = getHardcodedSteps();
     }
 
-
+    /**
+     * Updates the navigator with the current location.
+     * This method calculates the distance to the next step. If the distance is less then the THRESHOLD,
+     * it means that the step was reached and it moved on to the next one.
+     *
+     * @param location The current geographical location.
+     * @return True if navigation should continue, false if the destination is reached or an error occurs.
+     */
     public boolean update(LatLng location) {
         // Destination reached
         if (_currentStepIndex >= _steps.size())
             return false;
-
 
         //Calculate distance to next stop
         _distanceToNext = HaversineCalculator.haversineDistance(
@@ -83,16 +91,12 @@ public class NavigatorThingy {
 
 
         System.out.println("Step number: " + _currentStepIndex + " - delta distance: " + _distanceToNext);
-        System.out.println("Step number: " + _currentStepIndex + " - delta distance: " + _distanceToNext);
-        System.out.println("Step number: " + _currentStepIndex + " - delta distance: " + _distanceToNext);
 
         if(_distanceToNext > THRESHOLD)
             return false;
 
         // Got close to the waypoint -> change it
         _currentStepIndex++;
-        _changed = true;
-
 
         //RE-Calculate as it changed
         if (_currentStepIndex >= _steps.size()) {
@@ -107,19 +111,17 @@ public class NavigatorThingy {
             );
         }
 
-
         return true;
 
     }
 
-    /// todo change to update returning bool if it changed
 
-
-    //  ukazuje k step ke kteremu jedu!!!!
-
-
+    /**
+     * Retrieves the maneuver string for the current step.
+     *
+     * @return The maneuver as a string, or "destination" if the end is reached.
+     */
     public String getManeuverString() {
-        _changed = false;
 
         if (_currentStepIndex < _steps.size()) {
             return _steps.get(_currentStepIndex).maneuver;
@@ -129,26 +131,29 @@ public class NavigatorThingy {
         return DESTINATION_STR;
     }
 
-
+    /**
+     * Gets the maneuver ID corresponding to the current maneuver string.
+     *
+     * @return The maneuver ID, or -1 if not found.
+     */
     public int getManeuverID() {
         String maneuver = getManeuverString();
 
-        if(MANEUVER_ID.containsKey(maneuver))
-            return MANEUVER_ID.get(maneuver);
-        else
-            return -1;
+        return MANEUVER_ID.getOrDefault(maneuver, -1);
     }
 
+    /**
+     * Gets the distance to the next step in Kilometers.
+     *
+     * @return The distance to the next step, in kilometers.
+     */
     public double getDistanceToNext() {
         return _distanceToNext / 1000;
     }
 
 
-
-
-
-
-    private List<Step> getHardcodedSteps() {
+    // TODO remove
+    public static List<Step> getHardcodedSteps() {
 
         List<Step> list = new ArrayList<>();
 
@@ -189,14 +194,10 @@ public class NavigatorThingy {
 
         Step step8 = new Step();
         step8.endLocation = new LatLng(56.4632277, -2.973336);
-        step8.maneuver = "turn-right1";
+        step8.maneuver = "turn-right";
         list.add(step8);
 
 
         return list;
-
     }
-
-
-
 }
