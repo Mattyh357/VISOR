bc3d07ebe8261dd334608723e8c6fa4ebe8cfade
Scotchi <mattyh357@gmail.com>
2024-03-19 10:43:32 +0000

    Update VisorApplication.java
    Comments, refactoring, removing old methods

--- AndroidApp/visor/app/src/main/java/com/matt/visor/app/VisorApplication.java
+++ AndroidApp/visor/app/src/main/java/com/matt/visor/app/VisorApplication.java
@@ -1,5 +1,12 @@
-package com.matt.visor.app;
+/**
+ * This class is part of the V.I.S.O.R app.
+ * Manages application-level functionalities including device management, navigation data, and image processing.
+ *
+ * @version 1.0
+ * @since 21/02/2024
+ */
 
+package com.matt.visor.app;
 
 import android.app.Activity;
 import android.app.Application;
@@ -17,15 +24,20 @@ import java.util.Map;
 
 public class VisorApplication extends Application {
 
-
     public DeviceManager deviceManager = new DeviceManager();
     private Map<Integer, Bitmap> _listOfImages;
+    private List<Step> _steps;
+    private List<LatLng> _polylineRoute;
 
-    public boolean navigationEnabled = false;
 
-
+    /**
+     * Loads all saved sensors including gps, hud and additional sensors.
+     * This method currently has data hardcoded for current prototype, but these values should
+     * be saved in app's config.
+     *
+     * @param activity
+     */
     public void loadDevices(Activity activity) {
-        // TODO probably shouldn't be hardcoded :)
         String macAddress = "30:83:98:FB:D6:7A";
         String sUUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
         String cUUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
@@ -40,48 +52,59 @@ public class VisorApplication extends Application {
     }
 
 
-
-    private List<Step> _steps;
-    private List<LatLng> _polylineRoute;
-
+    /**
+     * Saves list of steps and polyline in the application class to ensure it persist when
+     * switching activities.
+     *
+     * @param steps List of steps
+     * @param polylineRoute Polyline
+     */
     public void saveNavigation(List<Step> steps, List<LatLng> polylineRoute) {
         _steps = steps;
         _polylineRoute = polylineRoute;
     }
 
+    /**
+     * Returns bool indicating whether or navigation was saved between activities.
+     *
+     * @return True if navigation route is saved.
+     */
     public boolean isNavigationSaved() {
         return _steps != null && _polylineRoute != null;
     }
 
+    /**
+     * Clears saved steps and polyline
+     */
     public void unSaveNavigation() {
         _steps = null;
         _polylineRoute = null;
     }
 
+    /**
+     * Returns list of steps representing used for navigation.
+     *
+     * @return List of steps
+     */
     public List<Step> getSteps() {
         return _steps;
     }
 
+    /**
+     * Returns list of LatLng representing polyline for navigation route.
+     *
+     * @return Polyline
+     */
     public List<LatLng> getPolylineRoute() {
         return _polylineRoute;
     }
 
 
-
-
-
-
-
-
-
-
-
-
     // Images for stuff
 
-
-
-
+    /**
+     * Loads and processes custom binary file, creates list of Bitmaps using CustomBinProcessor
+     */
     public void LoadImages() {
 
         File file = new File(this.getFilesDir(), "images.cbi");
@@ -96,18 +119,26 @@ public class VisorApplication extends Application {
         }
 
         System.out.println("done");
-
     }
 
+    /**
+     * Returns Bitmap of image corresponding Image ID (position in the list)
+     *
+     * @param id ID of the image
+     * @return Bitmap of the image
+     */
     public Bitmap getImageForId(int id) {
         return _listOfImages.get(id);
     }
 
+    /**
+     * Retrieves list of Bitmaps representing navigation icons
+     *
+     * @return List of images
+     */
     public List<Bitmap> get_listOfImages() {
         return new ArrayList<>(_listOfImages.values());
     }
-
-
 
 
 }
