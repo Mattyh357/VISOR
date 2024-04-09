0a6b88a8555c20c6a5fdc9755bfa425222703ef4
Matt <mattyh357@gmail.com>
2024-04-09 12:43:29 +0100

    SensorDetail2SensorDetailFragment

--- AndroidApp/visor/app/src/main/java/com/matt/visor/fragments/home/SensorDetailFragmet.java
+++ AndroidApp/visor/app/src/main/java/com/matt/visor/fragments/home/SensorDetailFragmet.java
@@ -4,14 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.matt.visor.app.MySensor;
+import com.matt.visor.app.VisorApplication;
 import com.matt.visor.databinding.FragmentSensorDetailBinding;
 
-public class SensorDetail extends Fragment {
+public class SensorDetailFragmet extends Fragment {
 
     private FragmentSensorDetailBinding _binding;
 
@@ -20,9 +21,18 @@ public class SensorDetail extends Fragment {
         _binding = FragmentSensorDetailBinding.inflate(inflater, container, false);
         View root = _binding.getRoot();
 
+
+        Bundle bundle = getArguments();
+        if(bundle != null){
+
+            VisorApplication app = (VisorApplication) requireActivity().getApplication();
+            int sensorType = bundle.getInt("SensorType");
+            MySensor sensor = app.deviceManager.getSensor(sensorType);
+
+            _binding.sensorDetailName.setText(sensor.getName());
+        }
+
         // meh
-        final TextView textView = _binding.text;
-        textView.setText("SensorDetail");
         return root;
     }
 
